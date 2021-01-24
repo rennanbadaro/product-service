a48ac05ac82aa91f2e1ece6d0abbfb9d922156a7
rennanbadaro <rennanrb@icloud.com>
2021-01-23 22:46:48 -0300

    feat: add use case to fetch products with discount

---
+++ src/domain/product/use-case/FetchProductsWithDiscount.test.ts
@@ -0,0 +1,68 @@
+import Product from '../Product';
+
+import FetchProductsWithDiscount from './FetchProductsWithDiscount';
+import ProductAdapter from '../../../infrastructure/adapters/ProductAdapter';
+
+let usecase: FetchProductsWithDiscount;
+let adapter: ProductAdapter;
+
+const productMock = new Product({
+  id: 'id',
+  priceInCents: 190,
+  title: 'mock product',
+  description: 'some fake product'
+});
+
+describe('FetchProductsWithDiscount', () => {
+  beforeEach(() => {
+    adapter = {
+      fetchProducts: jest.fn().mockResolvedValue([productMock]),
+      fetchDiscount: jest.fn().mockResolvedValue(null),
+    };
+
+    usecase = new FetchProductsWithDiscount(adapter);
+  });
+
+  describe('execute', () => {
+    it('Should return fetched products with no modification when no discount is returned', async () => {
+      const input = 'userId';
+
+      const result = await usecase.execute(input);
+
+      expect(adapter.fetchProducts).toHaveBeenCalledWith();
+      expect(adapter.fetchDiscount).toHaveBeenNthCalledWith(
+        1,
+        input,
+        productMock.id
+      );
+
+      expect(result).toEqual([productMock]);
+    });
+
+    it('Should return fetched products with applied discount', async () => {
+      const discountMock = {
+        percentage: 10,
+        valueInCents: productMock.priceInCents * 0.1,
+      };
+
+      adapter.fetchDiscount = jest.fn().mockResolvedValue(discountMock);
+
+      const input = 'userId';
+
+      const result = await usecase.execute(input);
+
+      expect(adapter.fetchProducts).toHaveBeenCalledWith();
+      expect(adapter.fetchDiscount).toHaveBeenNthCalledWith(
+        1,
+        input,
+        productMock.id
+      );
+
+      const expectedResult = [
+        new Product({ ...productMock, discount: discountMock }),
+      ];
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
---
+++ src/domain/product/use-case/FetchProductsWithDiscount.ts
@@ -0,0 +1,29 @@
+import ProductAdapter from '../../../infrastructure/adapters/ProductAdapter';
+import Discount from '../Discount';
+import Product from '../Product';
+
+class FetchProductsWithDiscount {
+  constructor(private readonly productAdapter: ProductAdapter) {}
+
+  async execute(userId: string) {
+    const products = await this.productAdapter.fetchProducts();
+
+    const productsWithDiscountPromises = products.map(product =>
+      this.productAdapter
+        .fetchDiscount(userId, product.id)
+        .then(discount => this.applyDiscount(product, discount))
+    );
+
+    return Promise.all(productsWithDiscountPromises);
+  }
+
+  private applyDiscount(product: Product, discount: Discount) {
+    if (discount === null) {
+      return product;
+    }
+
+    return new Product({ ...product, discount });
+  }
+}
+
+export default FetchProductsWithDiscount;
