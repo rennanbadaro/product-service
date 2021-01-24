d582128899cbe75248d527a102cf2e8f2716f274
rennanbadaro <rennanrb@icloud.com>
2021-01-24 01:04:11 -0300

    feat: implement ProductRepository

--- src/infrastructure/repositories/ProductRepository.ts
+++ src/infrastructure/repositories/ProductRepository.ts
@@ -1,7 +1,23 @@
+import Knex from 'knex';
 import Product from '../../domain/product/Product';
+import DataProvider from '../storage/DataProvider';
 
 interface IProductRepository {
   fetchAll(): Promise<Product[]>;
 }
 
-export { IProductRepository };
+class ProductRepository implements IProductRepository {
+  private readonly dbConn: Knex;
+
+  constructor(private readonly dbProvider: DataProvider) {
+    this.dbConn = dbProvider.getConnection();
+  }
+
+  async fetchAll() {
+    const result = await this.dbConn.select('*').from('product');
+
+    return result.map(r => new Product(r))
+  }
+}
+
+export { IProductRepository, ProductRepository };
