bafcc3aa7877ce650272d08cff7ffaa30db185ff
rennanbadaro <rennanrb@icloud.com>
2021-02-03 23:14:34 -0300

    feat: implement gRPC client

--- src/infrastructure/grpc/GrpcClient.ts
+++ src/infrastructure/grpc/GrpcClient.ts
@@ -1,7 +1,40 @@
+import grpc from 'grpc';
+import { promisify } from 'util';
+
 import Discount from '../../domain/product/Discount';
+import {
+  DiscountServiceClient,
+  IDiscountServiceClient,
+} from '../proto/discount_grpc_pb';
+import { GetDiscountRequest, GetDiscountResponse } from '../proto/discount_pb';
 
 interface IGrpcClient {
   getDiscount(userId: string, productId: string): Promise<Discount | null>;
 }
 
-export { IGrpcClient };
+class GrpcClient implements IGrpcClient {
+  constructor(private readonly discountClient: IDiscountServiceClient) {}
+
+  async getDiscount(userId: string, productId: string) {
+    const request = new GetDiscountRequest();
+    request.setUserId(userId);
+    request.setProductId(productId);
+
+    const get = promisify(this.discountClient.getDiscount).bind(
+      this.discountClient
+    );
+
+    let result: GetDiscountResponse;
+    try {
+      result = (await get(request)) as GetDiscountResponse;
+    } catch (err) {
+      console.error(err);
+
+      return null;
+    }
+
+    return result.toObject().discount || null;
+  }
+}
+
+export { GrpcClient, IGrpcClient };
